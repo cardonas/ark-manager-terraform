3ba69e793a1f4d748e9d0753d6a9620034071bb5
Steven Cardona <cardona.sj@gmail.com>
2024-12-15 08:10:20 -0600

    fix github action issue

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,9 +1,3 @@
-resource "google_storage_bucket" "create_tfstate_bucket" {
-  name          = var.state_bucket
-  location      = var.region
-  force_destroy = true
-}
-
 resource "google_storage_bucket" "bucket" {
   name     = var.bucket_name
   location = var.region
