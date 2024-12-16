6ca188d834769839cdc7de8b127e500991356550
Steven Cardona <cardona.sj@gmail.com>
2024-12-16 09:02:41 -0600

    prevent issue already exists issue

--- modules/service_account/main.tf
+++ modules/service_account/main.tf
@@ -1,4 +1,13 @@
+locals {
+  prevent = var.env == "ci" ? true : false
+}
+
+
 resource "google_service_account" "function_account" {
   account_id   = "ark-manager-func-sa-${var.env != "" ? var.env : "dev"}"
   display_name = "Cloud Function Service Account - ${var.env}"
+
+  lifecycle {
+    prevent_destroy = local.prevent
+  }
 }
--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,6 +1,14 @@
+locals {
+  prevent = var.env == "ci" ? true : false
+}
+
 resource "google_storage_bucket" "artifact_bucket" {
   name     = "ark-manager-artifacts-${var.env != "" ? var.env : "dev"}"
   location = var.region
+
+  lifecycle {
+    prevent_destroy = local.prevent
+  }
 }
 
 
